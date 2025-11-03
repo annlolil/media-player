e8e33cae18fdf8049ce1e95a45000d1136b6682a
Ann-Louis Lilja <lilja.annlouis@gmail.com>
2025-11-03 09:06:01 +0100

    Initial commit

---
+++ src/main/java/com/example/media_player/MediaPlayerApplication.java
@@ -0,0 +1,13 @@
+package com.example.media_player;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediaPlayerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediaPlayerApplication.class, args);
+	}
+
+}
