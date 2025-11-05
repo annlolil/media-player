9bea7e6c0c12a1ddbf9cf5967a03a02e1424b1c5
Ann-Louis Lilja <lilja.annlouis@gmail.com>
2025-11-05 14:41:53 +0100

    Added UserController class

---
+++ src/main/java/com/example/media_player/controllers/UserController.java
@@ -0,0 +1,9 @@
+package com.example.media_player.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/mediaplayer")
+public class UserController {
+}
